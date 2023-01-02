3ab25e8054a8ee1bc2742930f9890893925f0e8e
Dolfour <rodolfohenriques23@gmail.com>
2023-01-02 07:38:42 -0300

    Realizando Entrega

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,43 @@
-/* Monte sua lógica aqui */
+async function renderizaPokemons() {
+    const lista = document.querySelector(".pokemonList");
+
+    const listaDePokemons = await consomePokeAPI();
+
+    listaDePokemons.results.forEach(pokemon => {
+        const numeroNaPokedex = pokemon.url.slice(34, -1);
+
+        lista.insertAdjacentHTML("beforeend", `<li>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numeroNaPokedex}.png" alt=${pokemon.name}>
+        <h3>${pokemon.name[0].toUpperCase()}${pokemon.name.substring(1)}</h3></li>`)
+
+    
+    });
+}
+
+async function searchPokemon(searchWord){
+    const input = document.querySelector("#search");
+    const searchBtn = document.querySelector(".searchBtn");
+    const list = document.querySelector(".pokemonList");
+    searchWord = input.value
+
+    searchBtn.addEventListener("click", (event) => {
+        list.innerHTML = "";
+        console.log(searchWord)
+    })
+
+    const listaDePokemons = await consomePokeAPI();
+
+    listaDePokemons.results.forEach(pokemon => {
+        if(pokemon.name.includes(searchWord)){
+            const numeroNaPokedex = pokemon.url.slice(34, -1);
+            list.insertAdjacentHTML("beforeend", `<li>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${numeroNaPokedex}.png" alt=${pokemon.name}>
+        <h3>${pokemon.name[0].toUpperCase()}${pokemon.name.substring(1)}</h3></li>`)
+
+        }
+
+    });
+
+}
+
+renderizaPokemons();
--- src/scripts/requests.js
+++ src/scripts/requests.js
@@ -1,1 +1,19 @@
-/* Monte a lógica das requisições aqui */
+async function consomePokeAPI() {
+    const loading = document.querySelector('#loading');
+    const searchBar = document.querySelector("#searchBar");
+
+    const pokemonsDaAPI = await fetch('https://pokeapi.co/api/v2/pokemon')
+      .then(
+        response => response.json()
+      )
+      .catch(
+        error => console.log(error)
+      )
+
+    loading.classList.add('hidden')
+    searchBar.style = "display: flex;"
+
+    return pokemonsDaAPI
+}
+
+consomePokeAPI()
